35e2414e1a35799badda2408c6e1b878f2932e5f
kurutto <namihojo@gmail.com>
2025-06-24 14:22:12 +0900

    style:アプリ全体のスタイルを調整 (#8)

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -2,10 +2,10 @@ import { cn } from "@/lib/utils";
 
 interface headingProps
   extends Omit<React.ComponentPropsWithoutRef<"h1">, "className"> {
-  level?: 1 | 2 | 3 |4;
+  level?: 1 | 2 | 3 | 4;
   icon?: React.ElementType;
   outline?: boolean;
-  spBgWhite?:boolean;
+  spBgWhite?: boolean;
   children: React.ReactNode;
   className?: string;
 }
@@ -24,18 +24,17 @@ const Heading = ({
       "flex items-center justify-center font-bold md:text-2xl md:gap-4 max-md:text-xl max-md:gap-2.5",
     level === 2 &&
       cn(
-        "flex items-center font-bold md:text-xl md:gap-4 max-md:text-lg max-md:gap-2.5",
+        "flex items-center font-bold text-xl gap-4",
         outline &&
           "bg-white rounded-2xl md:py-5 md:px-7 max-md:py-3.5 max-md:px-3.5",
-        outline && (spBgWhite ? "shadow" : " md:shadow-pc max-md:shadow-sp"),
-
+        outline && (spBgWhite ? "shadow" : " md:shadow-pc max-md:shadow-sp")
       ),
-    level === 3 && "font-bold md:text-lg max-md:text-base",
+    level === 3 && "font-bold text-lg",
     level === 4 && "font-bold text-base"
   );
   const iconStyle = cn(
     level === 1 && "md:text-3xl max-md:text-2xl",
-    level === 2 && "md:text-2xl max-md:text-xl"
+    level === 2 && "text-2xl"
   );
 
   return (
