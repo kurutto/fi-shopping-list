9381e282dee73a87ab0f26ec2e7e4ab84aa169a8
kurutto <namihojo@gmail.com>
2025-07-14 11:32:30 +0900

    refactor:sort機能を簡略化

--- src/components/inventory/hooks/useHandleSort.ts
+++ src/components/inventory/hooks/useHandleSort.ts
@@ -1,76 +1,49 @@
 import { InventoryType } from "@/types/types";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const useHandleSort = (inventories: InventoryType[]) => {
   const [sortedInventories, setSortedInventories] =
     useState<InventoryType[]>(inventories);
+  const [sortConfig, setSortConfig] = useState<{
+    type: "remaining" | "kana";
+    order: "asc" | "desc";
+  }>();
   useEffect(() => {
     setSortedInventories(inventories);
   }, [inventories]);
 
+  useEffect(() => {
+    if (!sortConfig) return;
+    const newList = [...inventories];
+    let comparison = 0;
+    newList.sort((first, second) => {
+      if (sortConfig.type === "remaining") {
+        comparison = first.remaining - second.remaining;
+      } else if (sortConfig.type === "kana") {
+        comparison = first.kana.localeCompare(second.kana, "ja", {
+          sensitivity: "base",
+        });
+      }
+      return sortConfig.order === "desc" ? comparison : -comparison;
+    });
+    setSortedInventories(newList);
+  }, [inventories, sortConfig]);
   //残数でソート
-  const handleSortRemainingAscending = () => {
-    const newList = [...inventories];
-    newList.sort((first, second) => {
-      if (first.remaining > second.remaining) {
-        return -1;
-      } else if (second.remaining > first.remaining) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
-    setSortedInventories(newList);
-  };
-  const handleSortRemainingDescending = () => {
-    const newList = [...inventories];
-    newList.sort((first, second) => {
-      if (first.remaining < second.remaining) {
-        return -1;
-      } else if (second.remaining < first.remaining) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
-    setSortedInventories(newList);
-  };
+  const handleSortRemainingAscending = useCallback(() => {
+    setSortConfig({ type: "remaining", order: "asc" });
+  }, []);
+  const handleSortRemainingDescending = useCallback(() => {
+    setSortConfig({ type: "remaining", order: "desc" });
+  }, []);
 
   //カナでソート
-  const handleSortNameAscending = () => {
-    const newList = [...inventories];
-    newList.sort((first, second) => {
-      if (
-        first.kana.localeCompare(second.kana, "ja", { sensitivity: "base" }) > 0
-      ) {
-        return -1;
-      } else if (
-        first.kana.localeCompare(second.kana, "ja", { sensitivity: "base" }) < 0
-      ) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
-    setSortedInventories(newList);
-  };
-  const handleSortNameDescending = () => {
-    const newList = [...inventories];
-    newList.sort((first, second) => {
-      if (
-        first.kana.localeCompare(second.kana, "ja", { sensitivity: "base" }) > 0
-      ) {
-        return 1;
-      } else if (
-        first.kana.localeCompare(second.kana, "ja", { sensitivity: "base" }) < 0
-      ) {
-        return -1;
-      } else {
-        return 0;
-      }
-    });
-    setSortedInventories(newList);
-  };
+  const handleSortNameAscending = useCallback(() => {
+    setSortConfig({ type: "kana", order: "asc" });
+  }, []);
+  const handleSortNameDescending = useCallback(() => {
+    setSortConfig({ type: "kana", order: "desc" });
+  }, []);
+
   return {
     sortedInventories,
     handleSortRemainingAscending,
