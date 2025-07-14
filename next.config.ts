0d0ac0327a5ce40fbd0a1facea3cbf347f39b233
kurutto <namihojo@gmail.com>
2025-07-14 13:38:01 +0900

    refactor:next-pwaを@ducanh2912/next-pwaに変更

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
+import withPWA from "@ducanh2912/next-pwa";
 
 const withPWACustom = withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
@@ -13,4 +12,4 @@ const nextConfig: NextConfig = {
   output: "standalone",
 };
 
-export default withPWACustom(nextConfig as any);
+export default withPWACustom(nextConfig);
