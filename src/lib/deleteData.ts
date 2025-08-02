583ed89835c9105473dd337f425b3371370ec90e
kurutto <namihojo@gmail.com>
2025-08-02 12:45:29 +0900

    fix:vercelのエラーを修正

--- src/lib/deleteData.ts
+++ src/lib/deleteData.ts
@@ -2,7 +2,6 @@ export const deleteData = async (fetchPath: string) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${fetchPath}`, {
     method: "DELETE",
   });
-  const data = await res.json();
   if (!res.ok) {
     throw new Error(`Failed to fetch data: ${res.status}`);
   }
